e0c119d749d95fe430b39f11d03e27ecf9d3af71
PWilliams <66229742+PWilliamsDev@users.noreply.github.com>
2021-03-25 21:28:38 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector
-(".hamburger-menu");
-const snav = document.querySelector(".snav");
-
-menuIcon.addEventListener("click", () => {
-  snav.classList.toggle("change");
-});
